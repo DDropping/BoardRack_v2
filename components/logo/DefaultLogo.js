9a60b1230b9372e50b05f6e64379cbd593921ab6
David Dropping <ddropping@gmail.com>
2020-04-01 10:00:10 -0700

    Update logo components styling

--- components/logo/DefaultLogo.js
+++ components/logo/DefaultLogo.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export const Img = styled.img`
-  height: 100%;
-  width: 100%;
-`;
-
 export const ImgWrapper = styled.div`
-  display: inline-block;
+  position: absolute;
+  top: 0.5rem;
+  left: 0.5rem;
   flex: none;
   width: 200px;
   position: relative;
   color: inherit;
+`;
+
+export const Img = styled.img`
+  height: 100%;
+  width: 100%;
 `;
 
 const DefaultLogo = () => {
