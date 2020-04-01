8c55a4640b8cc3f7b9b785338e34439c0c740171
David Dropping <ddropping@gmail.com>
2020-03-31 20:08:14 -0700

    Create navbar links constants file

---
+++ constants/navbarLinks.js
@@ -0,0 +1,6 @@
+export default [
+  { title: 'Create Post', href: '/', protected: true },
+  { title: 'Login', href: '/', protected: false },
+  { title: 'Register', href: '/', protected: false },
+  { title: 'My Account', href: '/', protected: true }
+];
