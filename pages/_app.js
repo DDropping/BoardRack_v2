2ecb0d5d05d440a192c04a3c0fc9199c0c527f10
David Dropping <ddropping@gmail.com>
2020-04-01 10:00:42 -0700

    update global styling

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,20 @@ const GlobalStyle = createGlobalStyle`
   font-family: 'Roboto', sans-serif;
   padding: 0;
   margin: 0;
-}
+  text-decoration: none;
+  a{
+    color: ${props => props.theme.primaryBlack}
+    }
+  }
 `;
 
 const theme = {
-  colors: {
-    primaryBlue: '#00458a',
-    secondaryBlue: '#4878a9',
-    primaryWhite: '#ffffff',
-    primaryGrey: '#f3f7f9',
-    primaryGreen: '#52c41a'
-  }
+  primaryBlue: '#00458a',
+  secondaryBlue: '#4878a9',
+  primaryBlack: '#222222',
+  primaryWhite: '#ffffff',
+  primaryGrey: '#f3f7f9',
+  primaryGreen: '#52c41a'
 };
 
 export default class MyApp extends App {
