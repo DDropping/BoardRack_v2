4ad0ceac983064630d654ede8789798abc3c2c5c
David Dropping <ddropping@gmail.com>
2020-04-02 08:49:00 -0700

    Create createpost page route

---
+++ pages/createpost.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { Button } from 'antd';
+import styled from 'styled-components';
+
+const CreatePost = () => (
+  <div className="container">
+    <Head>
+      <title>BoardRack</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>Create Post</div>
+  </div>
+);
+
+export default CreatePost;
