3f960ff0b72a0f99ba0232c6182d01c8039a37d8
David Dropping <ddropping@gmail.com>
2020-04-03 16:18:40 -0700

    Create register component

---
+++ components/register/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Modal } from 'antd';
+
+import RegisterForm from './RegisterForm';
+
+const index = () => {
+  return (
+    <Modal
+      title="Register"
+      visible={true}
+      onOk={() => console.log('modal')}
+      onCancel={() => console.log('modal')}
+      footer={null}
+      zIndex={1100}
+    >
+      <RegisterForm />
+    </Modal>
+  );
+};
+
+export default index;
