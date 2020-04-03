8b246354c43d605c3d49735ac9b6cc9773f9144b
David Dropping <ddropping@gmail.com>
2020-04-03 09:01:03 -0700

    Add mor efields to createpost route

--- pages/api/posts/createpost.js
+++ pages/api/posts/createpost.js
@@ -10,27 +10,27 @@ export default async (req, res) => {
     title,
     price,
     boardType,
-    condition
-    // description,
-    // tail,
-    // finSystem,
-    // finConfiguration,
-    // lengthFt,
-    // lengthIn,
-    // width,
-    // depth,
-    // volume,
-    // construction,
-    // glassing,
-    // contour,
-    // waveSize,
-    // drive,
-    // paddlePower,
-    // movability,
-    // shaper,
-    // model,
-    // images,
-    // location
+    condition,
+    description,
+    tail,
+    finSystem,
+    finConfiguration,
+    lengthFt,
+    lengthIn,
+    width,
+    depth,
+    volume,
+    construction,
+    glassing,
+    contour,
+    waveSize,
+    drive,
+    paddlePower,
+    movability,
+    shaper,
+    model,
+    images,
+    location
   } = req.body;
 
   const postFields = {};
@@ -39,40 +39,39 @@ export default async (req, res) => {
   if (price) postFields.price = price;
   if (boardType) postFields.boardType = boardType;
   if (condition) postFields.condition = condition;
-  // if (description) postFields.description = description;
-  // if (tail) postFields.tail = tail;
-  // if (finSystem) postFields.finSystem = finSystem;
-  // if (finConfiguration) postFields.finConfiguration = finConfiguration;
-  // if (construction) postFields.construction = construction;
-  // if (glassing) postFields.glassing = glassing;
-  // if (contour) postFields.contour = contour;
-  // if (waveSize) postFields.waveSize = waveSize;
-  // if (drive) postFields.drive = drive;
-  // if (paddlePower) postFields.paddlePower = paddlePower;
-  // if (movability) postFields.movability = movability;
-  // if (shaper) postFields.shaper = shaper;
-  // if (model) postFields.model = model;
+  if (description) postFields.description = description;
+  if (tail) postFields.tail = tail;
+  if (finSystem) postFields.finSystem = finSystem;
+  if (finConfiguration) postFields.finConfiguration = finConfiguration;
+  if (construction) postFields.construction = construction;
+  if (glassing) postFields.glassing = glassing;
+  if (contour) postFields.contour = contour;
+  if (waveSize) postFields.waveSize = waveSize;
+  if (drive) postFields.drive = drive;
+  if (paddlePower) postFields.paddlePower = paddlePower;
+  if (movability) postFields.movability = movability;
+  if (shaper) postFields.shaper = shaper;
+  if (model) postFields.model = model;
 
-  // if (lengthFt) postFields.lengthFt = lengthFt;
-  // if (lengthIn) postFields.lengthIn = lengthIn;
-  // if (width) postFields.width = width;
-  // if (depth) postFields.depth = depth;
-  // if (volume) postFields.volume = volume;
+  if (lengthFt) postFields.lengthFt = lengthFt;
+  if (lengthIn) postFields.lengthIn = lengthIn;
+  if (width) postFields.width = width;
+  if (depth) postFields.depth = depth;
+  if (volume) postFields.volume = volume;
 
-  // //list of image urls
-  // if (images) postFields.images = images;
+  //list of image urls
+  if (images) postFields.images = images;
 
-  // //build location object
-  // postFields.location = {};
-  // if (location.lat) postFields.location.lat = location.lat;
-  // if (location.lng) postFields.location.lng = location.lng;
-  // if (location.country) postFields.location.country = location.country;
-  // if (location.state) postFields.location.state = location.state;
-  // if (location.city) postFields.location.city = location.city;
-  // if (location.postalCode)
-  //   postFields.location.postalCode = location.postalCode;
-  // if (location.locationImage)
-  //   postFields.location.locationImage = location.locationImage;
+  //build location object
+  postFields.location = {};
+  if (location.lat) postFields.location.lat = location.lat;
+  if (location.lng) postFields.location.lng = location.lng;
+  if (location.country) postFields.location.country = location.country;
+  if (location.state) postFields.location.state = location.state;
+  if (location.city) postFields.location.city = location.city;
+  if (location.postalCode) postFields.location.postalCode = location.postalCode;
+  if (location.locationImage)
+    postFields.location.locationImage = location.locationImage;
 
   try {
     let post = new Post(postFields);
