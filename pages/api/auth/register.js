a1198fb24a7323deee4c5c38f91a1f88cd5bb928
David Dropping <ddropping@gmail.com>
2020-04-03 17:04:28 -0700

    Create serverside validation for register route

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -6,26 +6,35 @@ import connectDb from '../../../utils/connectDb';
 import User from '../../../models/User';
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
 
 connectDb();
 
 export default async (req, res) => {
   const { username, email, password, confirmPassword, role } = req.body;
 
-  if (password !== confirmPassword) {
-    return res.status(400).json({
-      errors: [{ msg: 'Passwords do not match' }]
-    });
-  }
-
   try {
-    let user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({
-        errors: [{ msg: 'Email is already associated with an account' }]
-      });
+    //validate username email and password
+    if (!isLength(username, { min: 3, max: 15 })) {
+      return res.status(422).send('Username must be 3-15 characters');
+    } else if (!isLength(password, { min: 6 })) {
+      return res
+        .status(422)
+        .send('Password must be at least 6 characters long');
+    } else if (!isEmail(email)) {
+      return res.status(422).send('Invalid email');
+    } else if (password !== confirmPassword) {
+      return res.status(422).send('Passwords do not match');
     }
 
+    //check if user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(422).send('Email is already registered to an account');
+    }
+
+    //create user
     user = new User({
       username,
       email,
@@ -33,18 +42,20 @@ export default async (req, res) => {
       role
     });
 
+    //encrypt password
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
 
+    //save user to db
     await user.save();
 
+    //create JWT
     const payload = {
       user: {
         id: user.id,
         role: user.role
       }
     };
-
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
