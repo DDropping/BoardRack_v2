5ab370c70e7feb0b389d7d3b3c3b9fe7ad080a5e
David Dropping <ddropping@gmail.com>
2020-04-04 08:06:51 -0700

    Create redux store

---
+++ store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+export function initializeStore(initialState = {}) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+}
+
+export default initializeStore;
