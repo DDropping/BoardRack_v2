7909470831a575193a7b679fdbb14db24f886dd0
David Dropping <ddropping@gmail.com>
2020-04-12 21:01:44 -0700

    Update step 3 with map and location service

--- components/createPost/step3/index.js
+++ components/createPost/step3/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { Row, Col, Divider } from 'antd';
 
-const Step3 = ({ handlePostChange }) => {
+import GetLocationButton from './GetLocationButton';
+import GetLocationForm from './GetLocationForm';
+import Map from './Map';
+
+const H2 = styled.h2`
+  font-weight: bold;
+  color: ${props => props.theme.secondaryBlue};
+`;
+
+const LocationContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 0.5rem 0 1rem;
+  vertical-align: middle;
+`;
+
+const Disclaimer = styled.span`
+  color: ${({ theme }) => theme.primaryDarkGrey};
+  font-style: italic;
+`;
+
+const Step3 = () => {
   return (
     <div>
-      <div>step 3</div>
+      <Row>
+        <Divider>
+          <H2>Location</H2>
+        </Divider>
+        <Col xs={24} sm={12} md={12}>
+          <LocationContainer>
+            <GetLocationButton />
+            <Divider>OR</Divider>
+
+            <GetLocationForm />
+
+            <Disclaimer>*Your address will not be public</Disclaimer>
+          </LocationContainer>
+        </Col>
+        <Col xs={24} sm={12} md={12}>
+          <Map />
+        </Col>
+      </Row>
     </div>
   );
 };
