3515f806bb0207ccf87485e5eabf6a4fdadc15ef
David Dropping <ddropping@gmail.com>
2020-04-12 20:56:55 -0700

    Create get location form

---
+++ components/createPost/step3/GetLocationForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { Input } from 'antd';
+import { EnvironmentOutlined, LoadingOutlined } from '@ant-design/icons';
+
+import { TOGGLE_LOCATION_LOADING } from '../../../actions/types';
+import { handleLocationForm } from '../../../actions/location';
+
+const { Search } = Input;
+
+const GetLocationForm = () => {
+  const dispatch = useDispatch();
+  const isLocated = useSelector(state => state.currentLocation.isLocated);
+  const location = useSelector(state => state.currentLocation.location);
+
+  const handleGetLocation = value => {
+    dispatch(handleLocationForm({ value }));
+  };
+
+  return (
+    <Search
+      placeholder={
+        isLocated
+          ? location.city + ', ' + location.state + ' ' + location.postalCode
+          : 'Address, City, State...'
+      }
+      enterButton="Submit"
+      size="large"
+      onSearch={value => handleGetLocation(value)}
+    />
+  );
+};
+
+export default GetLocationForm;
