18b6ae2558077be554158adf3b5ebf3283d2621a
David Dropping <ddropping@gmail.com>
2020-04-12 20:58:14 -0700

    Add current location reducer to root reducer

--- reducers/index.js
+++ reducers/index.js
@@ -4,9 +4,11 @@ import auth from './auth';
 import overlays from './overlays';
 import createpost from './createpost';
 import imgUpload from './imgUpload';
+import currentLocation from './currentLocation';
 
 export default combineReducers({
   auth,
+  currentLocation,
   createpost,
   imgUpload,
   overlays
