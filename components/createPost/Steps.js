c9dd64529e81ce13e18317e3c5acd8f5360f3ceb
David Dropping <ddropping@gmail.com>
2020-04-19 07:34:18 -0700

    Update steps container

--- components/createPost/Steps.js
+++ components/createPost/Steps.js
@@ -5,46 +5,44 @@ const { Step } = Steps;
 
 const PostSteps = ({ step, handleStepChange }) => {
   return (
-    <div>
-      <Steps
-        type="navigation"
-        size="small"
-        current={step}
-        onChange={handleStepChange}
-        className="site-navigation-steps"
-      >
-        <Step
-          title="Create New Post"
-          subTitle=""
-          status={
-            (step === 0 && 'process') ||
-            (step === 1 && 'finish') ||
-            (step === 2 && 'finish')
-          }
-          description="Photos & Details"
-        />
-        <Step
-          title="Optional Details"
-          subTitle=""
-          status={
-            (step === 0 && 'wait') ||
-            (step === 1 && 'process') ||
-            (step === 2 && 'finish')
-          }
-          description="Dimensions & more"
-        />
-        <Step
-          title="Publish Post"
-          subTitle=""
-          status={
-            (step === 0 && 'wait') ||
-            (step === 1 && 'wait') ||
-            (step === 2 && 'process')
-          }
-          description="Location & Confirm"
-        />
-      </Steps>
-    </div>
+    <Steps
+      type="navigation"
+      size="small"
+      current={step}
+      onChange={handleStepChange}
+      className="site-navigation-steps"
+    >
+      <Step
+        title="Create New Post"
+        subTitle=""
+        status={
+          (step === 0 && 'process') ||
+          (step === 1 && 'finish') ||
+          (step === 2 && 'finish')
+        }
+        description="Photos & Details"
+      />
+      <Step
+        title="Optional Details"
+        subTitle=""
+        status={
+          (step === 0 && 'wait') ||
+          (step === 1 && 'process') ||
+          (step === 2 && 'finish')
+        }
+        description="Dimensions & more"
+      />
+      <Step
+        title="Publish Post"
+        subTitle=""
+        status={
+          (step === 0 && 'wait') ||
+          (step === 1 && 'wait') ||
+          (step === 2 && 'process')
+        }
+        description="Location & Confirm"
+      />
+    </Steps>
   );
 };
 
