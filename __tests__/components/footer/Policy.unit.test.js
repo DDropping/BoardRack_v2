64a6cbba51db83767b977d0c032f89c7637ec6f5
David Dropping <ddropping@gmail.com>
2020-04-23 07:46:21 -0700

    Create mock store for testing

--- __tests__/components/footer/Policy.unit.test.js
+++ __tests__/components/footer/Policy.unit.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 
 import Policy from '@components/footer/Policy';
+import { initializeStore } from '../../../store';
 
-let wrapped;
-beforeEach(() => {
-  wrapped = shallow(<Policy />);
-});
-
-afterEach(() => {
-  wrapped.unmount();
-});
+let store, wrapper;
+const initialState = {};
+store = initializeStore(initialState);
 
 it('has a list of policies', () => {
-  expect(wrapped.find('ul').length).toBe(1);
+  wrapper = mount(
+    <Provider store={store}>
+      <Policy />
+    </Provider>
+  );
+  expect(wrapper.find('ul').length).toBe(1);
 });
--- store.js
+++ store.js
@@ -3,15 +3,8 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
-
 const middleware = [thunk];
 
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// )
 let store;
 
 export function initializeStore(initialState = {}) {
