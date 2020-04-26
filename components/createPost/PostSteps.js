831068df328b4cdfefe8582e775583b29a1bea88
David <ddropping@gmail.com>
2020-04-26 09:53:06 -0700

    Update postSteps status prop

--- components/createPost/PostSteps.js
+++ components/createPost/PostSteps.js
@@ -15,19 +15,16 @@ const PostSteps = ({ step, handleStepChange }) => {
       <Step
         title="Create New Post"
         subTitle=""
-        {...(step === 0 ? (status = 'process') : (status = 'wait'))}
         description="Photos & Details"
       />
       <Step
         title="Optional Details"
         subTitle=""
-        {...(step === 1 ? (status = 'process') : (status = 'wait'))}
         description="Dimensions & more"
       />
       <Step
         title="Publish Post"
         subTitle=""
-        {...(step === 2 ? (status = 'process') : (status = 'wait'))}
         description="Location & Confirm"
       />
     </Steps>
