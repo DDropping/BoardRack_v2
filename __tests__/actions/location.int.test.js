e5f41607b492e5c537984ae8cb5cdef06e38dfb2
David <ddropping@gmail.com>
2020-04-27 12:42:55 -0700

    Test handleGeolocation action creator

--- __tests__/actions/location.int.test.js
+++ __tests__/actions/location.int.test.js
@@ -84,3 +84,38 @@ it("Loads location data into store by handleLocationForm()", () => {
       expect(newState.currentLocation).toStrictEqual(expectedState);
     });
 });
+
+it("Loads location data into store by handleGeolocation()", () => {
+  const expectedState = {
+    isLoading: false,
+    isLocated: true,
+    isLocatedWithIp: false,
+    isMapLoading: false,
+    location: {
+      lat: "test_lat",
+      lng: "test_lng",
+      country: "test_country",
+      state: "test_state",
+      county: "test_county",
+      city: "test_city",
+      district: "test_district",
+      postalCode: "test_postalCode",
+      locationImage: "test_locationImage",
+    },
+  };
+
+  moxios.wait(() => {
+    const request = moxios.requests.mostRecent();
+    request.respondWith({
+      status: 200,
+      response: expectedState.location,
+    });
+  });
+
+  return store
+    .dispatch(actions.handleGeolocation({ lat: 10, lng: 10 }))
+    .then(() => {
+      const newState = store.getState();
+      expect(newState.currentLocation).toStrictEqual(expectedState);
+    });
+});
