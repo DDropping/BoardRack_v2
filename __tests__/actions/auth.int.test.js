7985493ace2d8e3b746c31594f6a3ca2ac7d5f7d
David <ddropping@gmail.com>
2020-04-27 12:07:27 -0700

    Test loadUserByProps action

--- __tests__/actions/auth.int.test.js
+++ __tests__/actions/auth.int.test.js
@@ -14,22 +14,21 @@ let store = initializeStore(initialState);
 
 beforeEach(() => {
   moxios.install();
+  Cookie.set("token", "123123123123123123");
 });
 
 afterEach(() => {
   moxios.uninstall();
+  Cookie.remove("token");
 });
 
-it("Loads user data into store", () => {
+it("Loads user data into store by loadUserByCookie()", () => {
   const expectedState = {
     _id: "test_id",
     role: "user",
     username: "test_username",
     email: "test_name",
   };
-
-  jest.mock("js-cookie", () => jest.fn());
-  Cookie.set("token", "123123123123123123");
 
   moxios.wait(() => {
     const request = moxios.requests.mostRecent();
@@ -39,8 +38,22 @@ it("Loads user data into store", () => {
     });
   });
 
-  return store.dispatch(actions.loadUser()).then(() => {
+  return store.dispatch(actions.loadUserByCookie()).then(() => {
     const newState = store.getState();
     expect(newState.auth.user).toBe(expectedState);
+  });
+});
+
+it("Loads user data into store by loadUserByProps", () => {
+  const user = {
+    _id: "test_id",
+    role: "user",
+    username: "test_username",
+    email: "test_name",
+  };
+
+  return store.dispatch(actions.loadUserByProps(user)).then(() => {
+    const newState = store.getState();
+    expect(newState.auth.user).toBe(user);
   });
 });
