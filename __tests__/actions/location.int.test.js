0ef3d03a099bcaf96fb8ea33867200858b690910
David <ddropping@gmail.com>
2020-04-27 18:28:32 -0700

    Test Location action creators

--- __tests__/actions/location.int.test.js
+++ __tests__/actions/location.int.test.js
@@ -4,9 +4,6 @@ import Cookie from "js-cookie";
 import * as actions from "@actions/location";
 import { initializeStore } from "../../store";
 
-const initialState = {};
-let store = initializeStore(initialState);
-
 beforeEach(() => {
   moxios.install();
   Cookie.set("token", "123123123123123123");
@@ -17,105 +14,137 @@ afterEach(() => {
   Cookie.remove("token");
 });
 
-it("Loads location data into store by getLocationWithIp()", () => {
+describe("Location services", () => {
+  const initialState = {};
+  let store = initializeStore(initialState);
+
+  const expectedLocation = {
+    lat: "test_lat",
+    lng: "test_lng",
+    country: "test_country",
+    state: "test_state",
+    county: "test_county",
+    city: "test_city",
+    district: "test_district",
+    postalCode: "test_postalCode",
+    locationImage: "test_locationImage",
+  };
+
+  it("Loads location data into store by getLocationWithIp()", () => {
+    const expectedState = {
+      isLoading: false,
+      isLocated: false,
+      isLocatedWithIp: true,
+      isMapLoading: false,
+      location: expectedLocation,
+    };
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: expectedLocation,
+      });
+    });
+
+    return store.dispatch(actions.getLocationWithIp()).then(() => {
+      const newState = store.getState();
+      expect(newState.currentLocation).toStrictEqual(expectedState);
+    });
+  });
+
+  it("Loads location data into store by handleLocationForm()", () => {
+    const expectedState = {
+      isLoading: false,
+      isLocated: true,
+      isLocatedWithIp: false,
+      isMapLoading: false,
+      location: expectedLocation,
+    };
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: expectedLocation,
+      });
+    });
+
+    return store
+      .dispatch(actions.handleLocationForm("test value string"))
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.currentLocation).toStrictEqual(expectedState);
+      });
+  });
+
+  it("Loads location data into store by handleGeolocation()", () => {
+    const expectedState = {
+      isLoading: false,
+      isLocated: true,
+      isLocatedWithIp: false,
+      isMapLoading: false,
+      location: expectedLocation,
+    };
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: expectedLocation,
+      });
+    });
+
+    return store
+      .dispatch(actions.handleGeolocation({ lat: 10, lng: 10 }))
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.currentLocation).toStrictEqual(expectedState);
+      });
+  });
+});
+
+describe("Check to update user location", () => {
+  const initialState = {
+    auth: {
+      user: {
+        location: {
+          lat: 10,
+          lng: 10,
+        },
+      },
+    },
+  };
+  let store = initializeStore(initialState);
+
   const expectedState = {
     isLoading: false,
     isLocated: false,
-    isLocatedWithIp: true,
-    isMapLoading: false,
-    location: {
-      lat: "test_lat",
-      lng: "test_lng",
-      country: "test_country",
-      state: "test_state",
-      county: "test_county",
-      city: "test_city",
-      district: "test_district",
-      postalCode: "test_postalCode",
-      locationImage: "test_locationImage",
-    },
-  };
-
-  moxios.wait(() => {
-    const request = moxios.requests.mostRecent();
-    request.respondWith({
-      status: 200,
-      response: expectedState.location,
-    });
-  });
-
-  return store.dispatch(actions.getLocationWithIp()).then(() => {
-    const newState = store.getState();
-    expect(newState.currentLocation).toStrictEqual(expectedState);
-  });
-});
-
-it("Loads location data into store by handleLocationForm()", () => {
-  const expectedState = {
-    isLoading: false,
-    isLocated: true,
     isLocatedWithIp: false,
     isMapLoading: false,
     location: {
-      lat: "test_lat",
-      lng: "test_lng",
-      country: "test_country",
-      state: "test_state",
-      county: "test_county",
-      city: "test_city",
-      district: "test_district",
-      postalCode: "test_postalCode",
-      locationImage: "test_locationImage",
+      lat: null,
+      lng: null,
+      country: null,
+      state: "CA",
+      county: null,
+      city: "San Francisco",
+      district: null,
+      postalCode: "94121",
+      locationImage: null,
     },
   };
 
-  moxios.wait(() => {
-    const request = moxios.requests.mostRecent();
-    request.respondWith({
-      status: 200,
-      response: expectedState.location,
-    });
+  it("Dont update user's default location with checkToUpdateUserLocation()", () => {
+    return store
+      .dispatch(
+        actions.checkToUpdateUserLocation({
+          locationData: { lat: 10, lng: 10 },
+        })
+      )
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.currentLocation).toStrictEqual(expectedState);
+      });
   });
-
-  return store
-    .dispatch(actions.handleLocationForm("test value string"))
-    .then(() => {
-      const newState = store.getState();
-      expect(newState.currentLocation).toStrictEqual(expectedState);
-    });
-});
-
-it("Loads location data into store by handleGeolocation()", () => {
-  const expectedState = {
-    isLoading: false,
-    isLocated: true,
-    isLocatedWithIp: false,
-    isMapLoading: false,
-    location: {
-      lat: "test_lat",
-      lng: "test_lng",
-      country: "test_country",
-      state: "test_state",
-      county: "test_county",
-      city: "test_city",
-      district: "test_district",
-      postalCode: "test_postalCode",
-      locationImage: "test_locationImage",
-    },
-  };
-
-  moxios.wait(() => {
-    const request = moxios.requests.mostRecent();
-    request.respondWith({
-      status: 200,
-      response: expectedState.location,
-    });
-  });
-
-  return store
-    .dispatch(actions.handleGeolocation({ lat: 10, lng: 10 }))
-    .then(() => {
-      const newState = store.getState();
-      expect(newState.currentLocation).toStrictEqual(expectedState);
-    });
 });
