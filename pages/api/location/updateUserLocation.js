da295aba31257359c48dec19b2fcd44f056e0482
David <ddropping@gmail.com>
2020-04-30 13:09:25 -0700

    Update updateUserLocation with authenticate middleware

--- pages/api/location/updateUserLocation.js
+++ pages/api/location/updateUserLocation.js
@@ -1,13 +1,12 @@
-import jwt from 'jsonwebtoken';
-
-import connectDb from '../../../utils/ConnectDb';
-import User from '../../../models/User';
+import connectDb from "../../../utils/ConnectDb";
+import User from "../../../models/User";
+import authenticate from "../../../middleware/auth";
 
 connectDb();
 
-export default async (req, res) => {
+const handler = async (req, res) => {
   switch (req.method) {
-    case 'PUT':
+    case "PUT":
       await handlePutRequest(req, res);
       break;
     default:
@@ -21,23 +20,16 @@ export default async (req, res) => {
 // @res     -
 // @access  Protected
 async function handlePutRequest(req, res) {
-  if (!('authorization' in req.headers)) {
-    return res.status(401).send('No authorization token');
-  }
-
   try {
-    //get user id
-    const decodedUser = jwt.verify(
-      req.headers.authorization,
-      process.env.JWT_SECRET
-    );
     //update account
-    const id = decodedUser.user.id;
+    const id = req.user.id;
     const update = req.body;
     await User.findByIdAndUpdate(id, update);
-    res.json({ msg: 'Account Updated' });
+    res.json({ msg: "Account Updated" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 }
+
+export default authenticate(handler);
