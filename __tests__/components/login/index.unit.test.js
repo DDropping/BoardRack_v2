4c1abfdedbd209998788844b6133a8a9dbcd0720
David <ddropping@gmail.com>
2020-04-29 17:26:08 -0700

    Test login snapshot

---
+++ __tests__/components/login/index.unit.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Login from "@components/login";
+import { mount } from "enzyme";
+import renderer from "react-test-renderer";
+
+import { initializeStore } from "@store";
+import { Provider } from "react-redux";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+const initialState = {};
+const store = initializeStore(initialState);
+const wrapper = mount(
+  <Provider store={store}>
+    <Login />
+  </Provider>
+);
+
+it("matches snapshot", () => {
+  const tree = renderer.create(wrapper).toJSON();
+  expect(tree).toMatchSnapshot();
+});
