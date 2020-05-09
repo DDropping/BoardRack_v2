49114965e89eb1be1ce8e69374d96387e7470359
David Dropping <ddropping@gmail.com>
2020-05-09 12:06:01 -0700

    Create next prev arrows for carousel

--- components/carousel/index.js
+++ components/carousel/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Carousel } from 'antd';
 import styled from 'styled-components';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const CarouselContainer = styled.div`
   position: relative;
-  height: 200px;
+  height: 200px; /* controls height of unobstructed carosel */
 `;
 
 const CarouselWrapper = styled.div`
+  z-index: 1;
   .ant-carousel .slick-slide {
     text-align: center;
     line-height: 500px; /* controls height of total carousel */
@@ -31,11 +33,69 @@ const Img = styled.img`
   margin: auto;
 `;
 
+const ArrowWrapper = styled.div`
+  position: relative;
+  z-index: 2;
+`;
+
+const PreviousArrow = styled.div`
+  width: 100px;
+  height: 200px;
+  position: absolute;
+  left: 0;
+  font-size: 3rem;
+  padding: 65px 10px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(0, 69, 138);
+    background: linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 0.500437675070028) 0%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+`;
+
+const NextArrow = styled.div`
+  width: 100px;
+  height: 200px;
+  position: absolute;
+  right: 0;
+  font-size: 3rem;
+  padding: 65px 10px;
+  cursor: pointer;
+  &:hover {
+    background: rgb(0, 69, 138);
+    background: linear-gradient(
+      270deg,
+      rgba(0, 0, 0, 0.500437675070028) 0%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+`;
+
 const index = () => {
+  const carouselRef = useRef();
+
   return (
     <CarouselContainer>
+      <ArrowWrapper>
+        <PreviousArrow onClick={() => carouselRef.current.prev()}>
+          <LeftOutlined />
+        </PreviousArrow>
+      </ArrowWrapper>
+      <ArrowWrapper>
+        <NextArrow onClick={() => carouselRef.current.next()}>
+          <RightOutlined style={{ float: 'right' }} />
+        </NextArrow>
+      </ArrowWrapper>
       <CarouselWrapper>
-        <Carousel autoplay autoplaySpeed={7000} dotPosition="top">
+        <Carousel
+          ref={carouselRef}
+          autoplay
+          autoplaySpeed={7000}
+          dotPosition="top"
+        >
           <div>
             <ImgWrapper>
               <Img src="/images/banner/buysell.png" alt="boardrack logo" />
