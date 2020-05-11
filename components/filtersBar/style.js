fc9e360b6717b035e001de6ba9c7b919a76854c1
David <ddropping@gmail.com>
2020-05-11 12:50:50 -0700

    Update filtersBar style

--- components/filtersBar/style.js
+++ components/filtersBar/style.js
@@ -4,4 +4,6 @@ export const FiltersBarContainer = styled.div`
   display: flex;
   width: 100vw;
   background-color: ${({ theme }) => theme.primaryWhite};
+  border-bottom: 1px solid ${({ theme }) => theme.primaryGrey};
+  border-top: 1px solid ${({ theme }) => theme.primaryGrey};
 `;
--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,34 @@
-import App from 'next/app';
-import React from 'react';
-import Head from 'next/head';
-import axios from 'axios';
-import { ThemeProvider } from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
-import { parseCookies, destroyCookie } from 'nookies';
+import App from "next/app";
+import React from "react";
+import Head from "next/head";
+import axios from "axios";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { parseCookies, destroyCookie } from "nookies";
 
 //import 'antd/dist/antd.less' //import entire library styles
-import "antd/lib/avatar/style/index.js" // import specific styles
-import "antd/lib/button/style/index.js"
-import "antd/lib/card/style/index.js"
-import "antd/lib/carousel/style/index.js"
-import "antd/lib/checkbox/style/index.js"
-import "antd/lib/col/style/index.js"
-import "antd/lib/divider/style/index.js"
-import "antd/lib/drawer/style/index.js"
-import "antd/lib/form/style/index.js"
-import "antd/lib/grid/style/index.js"
-import "antd/lib/input/style/index.js"
-import "antd/lib/input-number/style/index.js"
-import "antd/lib/modal/style/index.js"
-import "antd/lib/notification/style/index.js"
-import "antd/lib/progress/style/index.js"
-import "antd/lib/row/style/index.js"
-import "antd/lib/select/style/index.js"
-import "antd/lib/steps/style/index.js"
+import "antd/lib/avatar/style/index.js"; // import specific styles
+import "antd/lib/button/style/index.js";
+import "antd/lib/card/style/index.js";
+import "antd/lib/carousel/style/index.js";
+import "antd/lib/checkbox/style/index.js";
+import "antd/lib/col/style/index.js";
+import "antd/lib/divider/style/index.js";
+import "antd/lib/drawer/style/index.js";
+import "antd/lib/form/style/index.js";
+import "antd/lib/grid/style/index.js";
+import "antd/lib/input/style/index.js";
+import "antd/lib/input-number/style/index.js";
+import "antd/lib/modal/style/index.js";
+import "antd/lib/notification/style/index.js";
+import "antd/lib/progress/style/index.js";
+import "antd/lib/row/style/index.js";
+import "antd/lib/select/style/index.js";
+import "antd/lib/steps/style/index.js";
 
-import Layout from '../components/layout';
-import { redirectUser } from '../utils/auth';
-import baseUrl from '../utils/baseUrl';
+import Layout from "../components/layout";
+import { redirectUser } from "../utils/auth";
+import baseUrl from "../utils/baseUrl";
 
 const GlobalStyle = createGlobalStyle`
 *{
@@ -37,7 +37,7 @@ const GlobalStyle = createGlobalStyle`
   margin: 0;
   text-decoration: none;
   a{
-    color: ${props => props.theme.primaryBlack}
+    color: ${(props) => props.theme.primaryBlack}
     }
     .ant-menu-item-selected{
       background-color: none;
@@ -47,32 +47,32 @@ const GlobalStyle = createGlobalStyle`
 
 const theme = {
   //colors
-  primaryBlue: '#00458a',
-  secondaryBlue: '#4878a9',
+  primaryBlue: "#00458a",
+  secondaryBlue: "#4878a9",
 
-  primaryRed: '#ef4040',
-  secondaryRed: '#ee7a7a',
+  primaryRed: "#ef4040",
+  secondaryRed: "#ee7a7a",
 
-  primaryBlack: '#222222',
+  primaryBlack: "#222222",
 
-  primaryWhite: '#ffffff',
-  secondaryWhite: '#eeeeee',
+  primaryWhite: "#ffffff",
+  secondaryWhite: "#eeeeee",
 
-  primaryGrey: '#f3f7f9',
-  primaryLightGrey: '#bbb',
-  primaryDarkGrey: '#949494',
+  primaryGrey: "#d9d9d9",
+  primaryLightGrey: "#bbb",
+  primaryDarkGrey: "#949494",
 
-  primaryGreen: '#52c41a',
+  primaryGreen: "#52c41a",
 
-  backgroundBlueMenu: '#4878a91f',
-  backgroundGreyMenu: '#5858581f',
-  backgroundRedMenu: '#ef40401f',
+  backgroundBlueMenu: "#4878a91f",
+  backgroundGreyMenu: "#5858581f",
+  backgroundRedMenu: "#ef40401f",
 
   //media sizes
-  sm: '576px', // Small devices (landscape phones, 576px and up)
-  md: '768px', // Medium devices (tablets, 768px and up)
-  lg: '992px', // Large devices (desktops, 992px and up)
-  xl: '1200px' // Extra large devices (large desktops, 1200px and up)
+  sm: "576px", // Small devices (landscape phones, 576px and up)
+  md: "768px", // Medium devices (tablets, 768px and up)
+  lg: "992px", // Large devices (desktops, 992px and up)
+  xl: "1200px", // Extra large devices (large desktops, 1200px and up)
 };
 
 export default class MyApp extends App {
@@ -88,13 +88,13 @@ export default class MyApp extends App {
     if (!token) {
       //redirect from protected routes if user not logged in
       const isProtectedRoute =
-        ctx.pathname === '/createpost' ||
-        ctx.pathname === '/account' ||
-        ctx.pathname === '/account/myposts' ||
-        ctx.pathname === '/account/mymessages' ||
-        ctx.pathname === '/account/myfavorites';
+        ctx.pathname === "/createpost" ||
+        ctx.pathname === "/account" ||
+        ctx.pathname === "/account/myposts" ||
+        ctx.pathname === "/account/mymessages" ||
+        ctx.pathname === "/account/myfavorites";
       if (isProtectedRoute) {
-        redirectUser(ctx, '/');
+        redirectUser(ctx, "/");
       }
     } else {
       try {
@@ -105,19 +105,19 @@ export default class MyApp extends App {
         const user = res.data;
 
         //redirect from admin dashboard if not authorized
-        const isRoot = user.role === 'root';
-        const isAdmin = user.role === 'admin';
+        const isRoot = user.role === "root";
+        const isAdmin = user.role === "admin";
         const isNotPermitted =
-          !(isRoot || isAdmin) && ctx.pathname === '/dashboard';
+          !(isRoot || isAdmin) && ctx.pathname === "/dashboard";
         if (isNotPermitted) {
-          redirectUser(ctx, '/');
+          redirectUser(ctx, "/");
         }
 
         //set user in page props
         pageProps.user = user;
       } catch (err) {
         console.log(err);
-        destroyCookie(ctx, 'token');
+        destroyCookie(ctx, "token");
       }
     }
 
