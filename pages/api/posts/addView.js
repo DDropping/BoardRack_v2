05777ceb232cbe7a1f8206cdaa2efd1bba0bbd07
David Dropping <ddropping@gmail.com>
2020-06-15 21:11:54 -0700

    Create addView route

---
+++ pages/api/posts/addView.js
@@ -0,0 +1,34 @@
+import connectDb from '../../../utils/ConnectDb';
+import Post from '../../../models/Post';
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case 'PUT':
+      await handlePutRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   PUT api/posts/addView
+// @desc    Increase viewcount of post by +1
+// @access  Public
+async function handlePutRequest(req, res) {
+  try {
+    const post = await Post.findById(req.body.id);
+
+    //increment post viewCount and save
+    post.viewCount++;
+    await post.save();
+    res.send('Post ' + req.body.id + ' opened.');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+}
+
+export default handler;
