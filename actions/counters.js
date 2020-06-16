730cefed5473dd556f64916025bf57e4afb1360a
David Dropping <ddropping@gmail.com>
2020-06-15 20:57:07 -0700

    Create addView action

--- actions/counters.js
+++ actions/counters.js
@@ -43,3 +43,26 @@ export const unfavorite = (id) => async (dispatch) => {
     }
   }
 };
+
+export const addView = (id) => async (dispatch) => {
+  //set headers for request
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  //stringify the form items
+  const postid = { postId: id };
+  const body = JSON.stringify(postid);
+
+  //update post to DB
+  try {
+    dispatch({ type: POST_VIEWED, payload: id });
+    await axios.put('/api/posts/addView', body, config);
+  } catch (err) {
+    if (err) {
+      console.log(err);
+    }
+  }
+};
