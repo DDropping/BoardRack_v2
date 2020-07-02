94ce4b271e9a7e0dda741ba035e87acbf6fff8f8
David Dropping <ddropping@gmail.com>
2020-07-02 10:37:43 -0700

    Create api/posts/postdetails/[id] route

---
+++ pages/api/posts/postdetails/[id].js
@@ -0,0 +1,33 @@
+import connectDb from '../../../../utils/ConnectDb';
+import Post from '../../../../models/Post';
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   Get api/posts/postdetails/:id
+// @desc    Retrieve post by id
+// @access  Public
+async function handleGetRequest(req, res) {
+  const {
+    query: { id },
+  } = req;
+  try {
+    const post = await Post.findById(id);
+    res.status(200).json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+}
+
+export default handler;
