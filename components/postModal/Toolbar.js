405ca35236c7654cd4cd47199382e1c045d19799
David <ddropping@gmail.com>
2020-07-04 11:31:44 -0700

    Update postModal toolbar with favorite functionality

--- components/postModal/Toolbar.js
+++ components/postModal/Toolbar.js
@@ -1,48 +1,67 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { useDispatch, useSelector } from 'react-redux';
-import { Tooltip } from 'antd';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { Tooltip } from "antd";
 import {
   StarOutlined,
   StarFilled,
   MailOutlined,
   CloseCircleOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 
-import { addFavorite, removeFavorite } from '../counters/util';
-import { Container, ToolbarButton, ToolbarButtonClose } from './stlye';
+import { addFavorite, removeFavorite } from "../counters/util";
+import { Container, ToolbarButton, ToolbarButtonClose } from "./stlye";
 
 const Toolbar = ({ postId }) => {
+  const [isFavorite, setFavorite] = useState(false);
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const user = useSelector((state) => state.auth.user);
   const router = useRouter();
 
+  useEffect(() => {
+    //check if user has favorited post
+    if (user) {
+      setFavorite(user.favorites.includes(postId));
+    }
+  }, [user]);
+
   function closeModal() {
-    router.push('/');
+    router.push("/");
   }
 
   return (
     <Container>
-      {isAuthenticated && (
-        <ToolbarButton onClick={() => addFavorite(postId)}>
+      {isAuthenticated && !isFavorite && (
+        <ToolbarButton
+          onClick={() => {
+            addFavorite(postId);
+            setFavorite(true);
+          }}
+        >
           <StarOutlined />
-          {' Favorite'}
+          {" Favorite"}
         </ToolbarButton>
       )}
-      {isAuthenticated && (
-        <ToolbarButton onClick={() => removeFavorite(postId)}>
-          <StarOutlined />
-          {' Unfavorite'}
+      {isAuthenticated && isFavorite && (
+        <ToolbarButton
+          onClick={() => {
+            removeFavorite(postId);
+            setFavorite(false);
+          }}
+        >
+          <StarFilled />
+          {" Unfavorite"}
         </ToolbarButton>
       )}
       <ToolbarButton>
         <MailOutlined />
-        {' Contact'}
+        {" Contact"}
       </ToolbarButton>
       <span style={{ flex: 1 }} />
       <ToolbarButtonClose onClick={() => closeModal()}>
         <CloseCircleOutlined />
-        {' Close'}
+        {" Close"}
       </ToolbarButtonClose>
     </Container>
   );
