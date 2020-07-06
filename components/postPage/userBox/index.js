f1633f684f39e0c38b69c66dce54cd4ce9402124
David <ddropping@gmail.com>
2020-07-06 11:48:52 -0700

    Create Message box for user box

--- components/postPage/userBox/index.js
+++ components/postPage/userBox/index.js
@@ -1,26 +1,60 @@
 import React, { useState } from "react";
-import { Avatar, Button } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, Input } from "antd";
+import { UserOutlined, PhoneOutlined, MailOutlined } from "@ant-design/icons";
 
 import {
   UserBoxContainer,
-  AvatarWrapper,
-  UserLocationContainer,
+  AvatarContainer,
+  DetailsContainer,
   ContactContainer,
+  MessageContainer,
 } from "./style";
+
+const { TextArea } = Input;
 
 const index = ({ user, location }) => {
   const [isContact, setIsContact] = useState(false);
+  const [message, setMessage] = useState("");
 
   return (
     <UserBoxContainer>
-      <AvatarWrapper>
-        <Avatar size={64} icon={<UserOutlined />} />
-      </AvatarWrapper>
-      <UserLocationContainer>
-        <div style={{ fontSize: "25px" }}>{user.username}</div>
+      <AvatarContainer>
+        <Avatar size={45} icon={<UserOutlined />} />
+      </AvatarContainer>
+      <DetailsContainer>
+        <strong style={{ fontSize: "20px", lineHeight: "20px" }}>
+          {user.username}
+        </strong>
         <div>{location.city + ", " + location.state}</div>
-      </UserLocationContainer>
+      </DetailsContainer>
+      <ContactContainer>
+        {!isContact && (
+          <Button type="primary" onClick={() => setIsContact(!isContact)}>
+            Contact
+          </Button>
+        )}
+        {isContact && (
+          <div>
+            <PhoneOutlined /> (831) 535-3535
+            <br />
+            <MailOutlined /> boards@boardrack.com
+          </div>
+        )}
+      </ContactContainer>
+      {isContact && (
+        <MessageContainer>
+          <TextArea
+            rows={4}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder={"Send " + user.username + " a message..."}
+            style={{ marginBottom: "10px" }}
+          />
+          <Button type="primary" block>
+            Send Message
+          </Button>
+        </MessageContainer>
+      )}
     </UserBoxContainer>
   );
 };
--- components/postPage/userBox/style.js
+++ components/postPage/userBox/style.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const UserBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   width: 100%;
   padding: 10px;
   margin-bottom: 10px;
@@ -10,12 +12,17 @@ export const UserBoxContainer = styled.div`
   background-color: #f2f5f9;
 `;
 
-export const AvatarWrapper = styled.div`
+export const AvatarContainer = styled.div`
   margin-right: 10px;
 `;
 
-export const UserLocationContainer = styled.div`
-  vertical-align: top;
+export const DetailsContainer = styled.div`
+  flex: 1;
 `;
 
 export const ContactContainer = styled.div``;
+
+export const MessageContainer = styled.div`
+  width: 100%;
+  margin-top: 10px;
+`;
