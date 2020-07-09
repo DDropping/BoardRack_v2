9d689c95d6b2b0b951532b2b247c2c60b3a1bc14
David <ddropping@gmail.com>
2020-07-09 14:35:55 -0700

    Create loading screen for displayPosts

--- components/displayPosts/index.js
+++ components/displayPosts/index.js
@@ -1,25 +1,29 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import { Container, Li } from "./style";
 import baseUrl from "../../utils/baseUrl";
 import PostCard from "../postCard";
 import PostModal from "../postModal";
-import { Container, Li } from "./style";
+import LoadingScreen from "../loadingScreens/displayPosts";
 
 const index = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const url = `${baseUrl}/api/posts/postdetails`;
       const res = await axios.get(url);
       setPosts(res.data);
+      setLoading(false);
     }
     fetchData();
   }, []);
 
   return (
     <Container>
+      {isLoading && <LoadingScreen />}
       <PostModal quickData={posts} />
       {posts.map((post, index) => {
         return (
---
+++ components/loadingScreens/displayPosts/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, TextWrapper } from "./style";
+
+const index = () => {
+  return (
+    <Container>
+      <TextWrapper>Checking our board rack...</TextWrapper>
+    </Container>
+  );
+};
+
+export default index;
