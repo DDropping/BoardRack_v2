0aa68847856842a35758b1017a138fd30286a6da
David <ddropping@gmail.com>
2020-07-22 16:26:38 -0700

    Update account menu style

--- components/account/menu/index.js
+++ components/account/menu/index.js
@@ -1,17 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import { Menu } from "antd";
 
-import { Container } from "./style";
+import { Container, A, Logout } from "./style";
+import { LogoutOutlined } from "@ant-design/icons";
+import accountLinks from "../../../constants/accountLinks";
 
 const index = () => {
   return (
     <Container>
-      BigDawg San Francisco, CA
-      <ul>
-        <li>Overview</li>
-        <li>My Boardrack</li>
-        <li>Favorites</li>
-        <li>Messages</li>
-      </ul>
+      <Menu>
+        {accountLinks.map((item, index) => {
+          return (
+            <Menu.Item key={index} style={{ padding: 0, margin: 0 }}>
+              <Link href={item.href}>
+                <A href="/" key={index}>
+                  {item.icon} {item.title}
+                </A>
+              </Link>
+            </Menu.Item>
+          );
+        })}
+        <Menu.Divider style={{ margin: "0 10px" }} />
+        <Menu.Item
+          key="logout"
+          onClick={() => {
+            logoutModal(handleLogout);
+          }}
+          style={{ padding: 0, margin: 0 }}
+        >
+          <Logout href="">
+            <LogoutOutlined /> Logout
+          </Logout>
+        </Menu.Item>
+      </Menu>
     </Container>
   );
 };
--- components/account/menu/style.js
+++ components/account/menu/style.js
@@ -2,6 +2,34 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 200px;
-  min-height: 500px;
-  border-right: 1px solid ${({ theme }) => theme.primaryGrey};
+  .ant-menu:not(.ant-menu-horizontal) .ant-menu-item-selected {
+    background-color: ${({ theme }) => theme.backgroundBlueMenu};
+  }
+`;
+
+export const A = styled.a`
+  padding: 0 0.5rem;
+  transition: all 0.2s ease-in-out;
+  ${({ active, theme }) =>
+    active &&
+    `background-color: ${theme.backgroundBlueMenu}; 
+  border-right: 2px solid ${theme.primaryBlue}; 
+  padding: 1rem;`}
+  &:hover {
+    background-color: ${({ theme }) => theme.backgroundBlueMenu};
+    border-right: 2px solid ${({ theme }) => theme.primaryBlue};
+    color: ${({ theme }) => theme.primaryBlue};
+    padding-left: 1rem;
+  }
+`;
+
+export const Logout = styled.div`
+  padding: 0 0.5rem;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: ${({ theme }) => theme.backgroundRedMenu};
+    border-right: 2px solid ${({ theme }) => theme.primaryRed};
+    color: ${({ theme }) => theme.primaryRed};
+    padding-left: 1rem;
+  }
 `;
