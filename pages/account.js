36f6c644d630f8d5534690c53141342bb205ded0
David <ddropping@gmail.com>
2020-07-21 18:30:05 -0700

    Create header component for account page

--- pages/account.js
+++ pages/account.js
@@ -1,13 +1,21 @@
-import Head from 'next/head';
-import styled from 'styled-components';
-import { Button } from 'antd';
+import Head from "next/head";
+import styled from "styled-components";
+
+import Header from "../components/account/header";
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 const Account = () => (
   <div className="container">
     <Head>
       <title>BoardRack | Account</title>
     </Head>
-    <div>Account Page</div>
+    <Container>
+      <Header />
+    </Container>
   </div>
 );
 
