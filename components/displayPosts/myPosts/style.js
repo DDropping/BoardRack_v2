c89ac42e6414c0a3152f17cfa4a81a4c230f8428
David <ddropping@gmail.com>
2020-07-27 16:39:10 -0700

    Create myPosts component

---
+++ components/displayPosts/myPosts/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: inline-block;
+  vertical-align: top;
+  flex: 1;
+  li {
+    list-style-type: none;
+    a {
+      :hover {
+        color: ${({ theme }) => theme.primaryBlack};
+      }
+    }
+  }
+`;
+
+export const Li = styled.li`
+  display: inline-block;
+`;
