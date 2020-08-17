647678fae0a6c2a0a74879d808157dd688be801a
David <ddropping@gmail.com>
2020-08-17 14:23:24 -0700

    Update account/post component to display user favorites

--- components/account/favorites/index.js
+++ components/account/favorites/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Divider } from "antd";
+
+import { headerStyle } from "../style";
+import MyFavorites from "../../displayPosts/myFavorites";
 
 const index = () => {
   return (
     <div>
-      <div>favorites</div>
+      <Divider orientation="left" style={headerStyle}>
+        My Favorites
+      </Divider>
+      <MyFavorites />
     </div>
   );
 };
