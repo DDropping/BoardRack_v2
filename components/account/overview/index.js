64da7a34a3e52cfb6d87ef93c892f70fb63893f9
David <ddropping@gmail.com>
2020-08-18 18:16:09 -0700

    Update overview with navigation buttons

--- components/account/overview/index.js
+++ components/account/overview/index.js
@@ -3,7 +3,6 @@ import { Divider } from "antd";
 
 import { Container, SectionContainer, Separator } from "./style";
 import { headerStyle } from "../style";
-import ViewAllButton from "../ViewAllButton";
 import MyPosts from "../../displayPosts/myPosts";
 import MyFavorites from "../../displayPosts/myFavorites";
 
@@ -14,16 +13,16 @@ const index = () => {
         <Divider orientation="left" style={headerStyle}>
           My Boardrack
         </Divider>
-        <ViewAllButton link="/account?view=posts" />
-        <MyPosts />
+        <MyPosts preview={true} />
       </SectionContainer>
+
       <Separator />
+
       <SectionContainer>
         <Divider orientation="left" style={headerStyle}>
           My Favorites
         </Divider>
-        <ViewAllButton link="/account?view=posts" />
-        <MyFavorites />
+        <MyFavorites preview={true} />
       </SectionContainer>
     </Container>
   );
