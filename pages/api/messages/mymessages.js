c95a739a12fc3505ab692959ac835ad8052f1be9
David <ddropping@gmail.com>
2020-08-25 18:18:25 -0700

    Create myMessages api route

---
+++ pages/api/messages/mymessages.js
@@ -0,0 +1,44 @@
+import connectDb from "../../../utils/ConnectDb";
+import Message from "../../../models/Message";
+import "../../../models/User";
+import "../../../models/Post";
+import authenticate from "../../../middleware/auth";
+import mongoose from "mongoose";
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await handleGetRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   GET api/posts/postdetails/myposts
+// @desc    retrieve all posts that user has created
+// @res     posts: {... array of all posts}
+// @access  Public
+async function handleGetRequest(req, res) {
+  try {
+    const messages = await Message.find({
+      users: { $in: new mongoose.Types.ObjectId(req.user.id) },
+    })
+      .populate("post", "title price")
+      .populate("users", "username");
+
+    if (!messages) {
+      return res.status(400).json({ msg: "There are no Messages" });
+    }
+
+    res.status(200).json(messages);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+}
+
+export default authenticate(handler);
