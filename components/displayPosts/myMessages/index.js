1e08cb75ab0bd36c0f864f752794f78826f85c25
David <ddropping@gmail.com>
2020-08-26 16:55:39 -0700

    Update message overview display limit

--- components/displayPosts/myMessages/index.js
+++ components/displayPosts/myMessages/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import { AppstoreOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import Link from "next/link";
 
-import { Container } from "./style";
+import { Container, ButtonContainer, MessagesContainer } from "./style";
 import baseUrl from "../../../utils/baseUrl";
 import MessageCard from "../../messageCard";
 
@@ -20,7 +23,7 @@ const index = () => {
     async function fetchData() {
       const url = `${baseUrl}/api/messages/mymessages`;
       const res = await axios.get(url);
-      setMessages(res.data);
+      setMessages(res.data.slice(0, 5));
       setLoading(false);
     }
     if (isAuthenticated) {
@@ -30,11 +33,24 @@ const index = () => {
 
   return (
     <Container>
-      {!isLoading &&
-        messages.length > 0 &&
-        messages.map((messageThread, index) => {
-          return <MessageCard messageThread={messageThread} key={index} />;
-        })}
+      {messages.length > 0 && (
+        <ButtonContainer>
+          <div style={{ flex: 1 }} />
+          <Link href="/account?view=messages" shallow={true}>
+            <Button style={{ marginRight: "10px" }}>
+              <AppstoreOutlined /> View All messages
+            </Button>
+          </Link>
+        </ButtonContainer>
+      )}
+
+      <MessagesContainer>
+        {!isLoading &&
+          messages.length > 0 &&
+          messages.map((messageThread, index) => {
+            return <MessageCard messageThread={messageThread} key={index} />;
+          })}
+      </MessagesContainer>
     </Container>
   );
 };
--- components/displayPosts/myMessages/style.js
+++ components/displayPosts/myMessages/style.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+export const MessagesContainer = styled.div`
   display: flex;
   margin: 10px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
 `;
