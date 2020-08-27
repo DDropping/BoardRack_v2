d53f6a5ce46d9efc136d170fd31a7c2011ba1c56
David <ddropping@gmail.com>
2020-08-27 10:04:47 -0700

    Update messages account page components

--- components/account/messages/index.js
+++ components/account/messages/index.js
@@ -1,11 +1,54 @@
-import React from "react";
-import Message from "../../messageCard";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+import timeAgo from "../../../utils/timeAgo";
+import { Container } from "./style";
+import baseUrl from "../../../utils/baseUrl";
+import MessageList from "./messageList";
+import MessageThread from "./messageThread";
 
 const index = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const user = useSelector((state) => state.auth.user);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = `${baseUrl}/api/messages/mymessages`;
+      const res = await axios.get(url);
+
+      //add from and timeago elements
+      var messages = res.data.map((e) => {
+        e.from = e.users.filter(
+          (userDetails) => userDetails._id !== user._id
+        )[0];
+        e.timeAgo = timeAgo(e.messages[0].timeSent);
+        return e;
+      });
+
+      setMessages(messages);
+      setLoading(false);
+    }
+    if (isAuthenticated) {
+      fetchData();
+    }
+  }, [isAuthenticated]);
+
+  console.log(messages);
+
   return (
-    <div>
-      <Message />
-    </div>
+    <Container>
+      <MessageList messages={messages} isLoading={isLoading} />
+      <MessageThread
+        messageData={messages.find((messageDetails) => {
+          return messageDetails._id === router.query.thread;
+        })}
+      />
+    </Container>
   );
 };
 
