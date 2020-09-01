2d2588cdf7511961303ead01636cc46866d4acf9
David <ddropping@gmail.com>
2020-09-01 16:41:43 -0700

    Create PostListRow component

---
+++ components/postListRow/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Container } from "./style";
+import Images from "./Images";
+import Content from "./Content";
+import Counters from "./Counters";
+
+const index = ({ postData }) => {
+  console.log("postListRow: ", postData);
+  const countersData = {
+    postId: postData._id,
+    price: postData.price,
+    favorites: postData.favorites,
+    viewCount: postData.viewCount,
+  };
+  const imageData = {
+    image: postData.images[0],
+  };
+  const contentData = {
+    title: postData.title,
+    lengthFt: postData.lengthFt,
+    lengthIn: postData.lengthIn,
+    width: postData.width,
+    depth: postData.depth,
+    volume: postData.volume,
+    condition: postData.condition,
+    location: postData.location,
+    shaper: postData.shaper,
+    model: postData.model,
+  };
+
+  return (
+    <Container>
+      <Images data={imageData} />
+      <Content data={contentData} />
+      <Counters data={countersData} />
+    </Container>
+  );
+};
+
+export default index;
