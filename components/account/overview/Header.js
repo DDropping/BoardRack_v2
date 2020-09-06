6045fef294bbfe963883b6c464aa685c0206af0c
David <ddropping@gmail.com>
2020-09-06 12:27:32 -0700

    Create header component for account overview section

---
+++ components/account/overview/Header.js
@@ -0,0 +1,30 @@
+import react from "react";
+import { Button } from "antd";
+import { AppstoreOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  font-size: 1.25rem;
+`;
+
+const Header = ({ title, link, buttonText }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <div style={{ flex: 1 }} />
+      <Link href={link} shallow={true}>
+        <Button type="primary">
+          <AppstoreOutlined /> {buttonText}
+        </Button>
+      </Link>
+    </Container>
+  );
+};
+
+export default Header;
