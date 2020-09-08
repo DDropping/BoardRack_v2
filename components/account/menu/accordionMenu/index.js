db7379f067ab9af97bc596ae3ddf961bdef4af10
David <ddropping@gmail.com>
2020-09-08 16:39:07 -0700

    Create responsive account menu

---
+++ components/account/menu/accordionMenu/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { Divider } from "antd";
+import { DownOutlined, RightOutlined } from "@ant-design/icons";
+
+import { Container, Header, Ul, Li } from "./style";
+import { DEAUTH_USER } from "../../../../actions/types";
+import { LogoutOutlined } from "@ant-design/icons";
+import accountLinks from "../../../../constants/accountLinks";
+import logoutModal from "../../../logout";
+
+const index = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [isOpen, toggleIsOpen] = useState(false);
+  const [menuSelected, setMenuSelected] = useState(
+    router.query.view ? router.query.view : "overview"
+  );
+
+  const handleLogout = () => {
+    dispatch({ type: DEAUTH_USER });
+    router.reload(window.location.pathname);
+  };
+
+  return (
+    <Container>
+      <Header onClick={() => toggleIsOpen(!isOpen)}>
+        {isOpen ? <RightOutlined /> : <DownOutlined />}{" "}
+        {accountLinks.filter((item) => item.view === menuSelected)[0].icon}{" "}
+        {accountLinks.filter((item) => item.view === menuSelected)[0].title}
+      </Header>
+      <Ul isOpen={isOpen}>
+        {accountLinks.map((item, index) => {
+          return (
+            <Link href={item.href} shallow={true} key={index}>
+              <Li
+                active={router.query.view === item.view}
+                onClick={() => {
+                  setMenuSelected(item.view);
+                  toggleIsOpen(!isOpen);
+                }}
+              >
+                {item.icon} {item.title}
+              </Li>
+            </Link>
+          );
+        })}
+        <Divider style={{ margin: "2px 0 0 0" }} />
+        <Li
+          key="logout"
+          isLogout={true}
+          onClick={() => {
+            logoutModal(handleLogout);
+          }}
+        >
+          <LogoutOutlined /> Logout
+        </Li>
+      </Ul>
+    </Container>
+  );
+};
+
+export default index;
---
+++ components/account/menu/accordionMenu/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 10px 10px 0 10px;
+  cursor: pointer;
+  @media (min-width: ${({ theme }) => theme.md1}) {
+    display: none;
+  }
+`;
+
+export const Header = styled.div`
+  font-size: 1.15rem;
+`;
+
+export const Ul = styled.ul`
+  list-style-type: none;
+  ${({ isOpen }) => isOpen && { display: "none" }}
+`;
+
+export const Li = styled.li`
+  cursor: pointer;
+  padding: 10px;
+  margin-bottom: 1px;
+  transition: ${({ theme }) => theme.easeInOut};
+  ${({ active, theme }) =>
+    active && {
+      backgroundColor: theme.backgroundBlueMenu,
+      borderLeft: `2px solid ${theme.primaryBlue}`,
+    }}
+  &:hover {
+    background-color: ${({ theme, isLogout }) =>
+      isLogout ? theme.backgroundRedMenu : theme.backgroundBlueMenu};
+    color: ${({ theme, isLogout }) =>
+      isLogout ? theme.primaryRed : theme.primaryBlue};
+    padding-left: 20px;
+  }
+`;
