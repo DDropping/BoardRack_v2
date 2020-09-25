40ecaee5f4531027814713b0410f8f437837fcdd
David <ddropping@gmail.com>
2020-09-24 19:30:34 -0700

    Create updateAccount route

---
+++ pages/api/auth/updateAccount.js
@@ -0,0 +1,78 @@
+import connectDb from "../../../utils/ConnectDb";
+import User from "../../../models/User";
+const bcrypt = require("bcryptjs");
+import isEmail from "validator/lib/isEmail";
+import authenticate from "../../../middleware/auth";
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case "PATCH":
+      await handlePatchRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   PATCH api/auth/register
+// @desc    register new account in db (username, email, password, role)
+// @res     jwt
+// @access  Public
+async function handlePatchRequest(req, res) {
+  const { password, newPassword } = req.body;
+
+  const userId = req.user.id;
+  var updates = {};
+  const options = { new: true };
+
+  var passwordsMatch = false;
+
+  console.log("req body: ");
+  console.log(req.body);
+
+  try {
+    //check if user exists
+    const user = await User.findById(userId).select("+password");
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+
+    console.log("user found");
+    console.log(user);
+
+    // if password change request, verify credentials
+    if (password && newPassword) {
+      passwordsMatch = await bcrypt.compare(password, user.password);
+
+      //encrypt new password and add to updates object
+      const salt = await bcrypt.genSalt(10);
+      updates.password = await bcrypt.hash(newPassword, salt);
+
+      if (!passwordsMatch) {
+        return res.status(401).send("Invalid Credentials");
+      }
+    }
+    console.log(passwordsMatch);
+    console.log("passwords match");
+
+    //Create updates object
+    if (isEmail(req.body.email)) updates.email = req.body.email;
+    if (req.body.profileImage) updates.profileImage = req.body.profileImage;
+
+    console.log("update object");
+    console.log(updates);
+
+    //update user data
+    const result = await User.findByIdAndUpdate(userId, updates, options);
+    console.log("success");
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+}
+
+export default authenticate(handler);
