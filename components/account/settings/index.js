c05c37dad8d903054af97101bc0ca82ebc706e7e
David <ddropping@gmail.com>
2020-09-24 19:32:20 -0700

    Update userData initial state

--- components/account/settings/index.js
+++ components/account/settings/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Button } from "antd";
+import baseUrl from "../../../utils/baseUrl";
+import axios from "axios";
 
 import { Container, ButtonsContainer, ButtonText } from "./style";
 import MyAccount from "./MyAccount";
@@ -10,16 +12,30 @@ import ManageAccount from "./ManageAccount";
 
 const index = () => {
   const initialState = {
-    username: "",
     email: "",
-    oldPassword: "",
+    password: "",
     newPassword: "",
     newPasswordConfirm: "",
-    defaultLocation: "",
+    profileImage: "",
   };
 
   const user = useSelector((state) => state.auth.user);
   const [userData, setUserData] = useState(initialState);
+
+  const updateUserData = async () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    //stringify the form items
+    const body = JSON.stringify(userData);
+
+    const url = `${baseUrl}/api/auth/updateAccount`;
+    const res = await axios.patch(url, body, config);
+    console.log(res);
+  };
 
   return (
     <Container>
@@ -32,7 +48,9 @@ const index = () => {
             ? "*Changes are not currently saved"
             : "*Account Up to Date"}
         </ButtonText>
-        <Button type="primary">Save Changes</Button>
+        <Button type="primary" onClick={updateUserData}>
+          Save Changes
+        </Button>
       </ButtonsContainer>
       <MyAccount user={user} userData={userData} setUserData={setUserData} />
       <Location user={user} userData={userData} setUserData={setUserData} />
