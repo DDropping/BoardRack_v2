62b31f7cb45e3b5b5cb2a5cbbaee1daa8a9000bb
David <ddropping@gmail.com>
2020-09-28 20:46:44 -0700

    Delete unused imports

--- components/account/header/index.js
+++ components/account/header/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { UserOutlined } from "@ant-design/icons";
 import { useRouter } from "next/router";
 
 import { Container, Banner, AvatarWrapper } from "./style";
