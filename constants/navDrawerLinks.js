15f250dad56b1fb9f998f6c81825a07379d6f01e
David <ddropping@gmail.com>
2020-10-14 16:15:18 -0700

    Update navDrawerLinks

--- constants/navDrawerLinks.js
+++ constants/navDrawerLinks.js
@@ -9,35 +9,52 @@ import {
 } from "@ant-design/icons";
 
 export default [
-  { title: "Home", href: "/", icon: <HomeOutlined />, protected: false },
-  { title: "Home", href: "/", icon: <HomeOutlined />, protected: true },
+  {
+    title: "Home",
+    href: "/",
+    view: "",
+    icon: <HomeOutlined />,
+    protected: false,
+  },
+  {
+    title: "Home",
+    href: "/",
+    view: "",
+    icon: <HomeOutlined />,
+    protected: true,
+  },
   {
     title: "My Account",
     href: "/account?view=overview",
+    view: "overview",
     icon: <UserOutlined />,
     protected: true,
   },
   {
     title: "My Boardrack",
     href: "/account?view=posts",
+    view: "posts",
     icon: <FormOutlined />,
     protected: true,
   },
   {
     title: "My Favorites",
     href: "/account?view=favorites",
+    view: "favorites",
     icon: <StarOutlined />,
     protected: true,
   },
   {
     title: "My Messages",
     href: "/account?view=messages",
+    view: "messages",
     icon: <MailOutlined />,
     protected: true,
   },
   {
     title: "Account Settings",
     href: "/account?view=settings",
+    view: "settings",
     icon: <SettingOutlined />,
     protected: true,
   },
