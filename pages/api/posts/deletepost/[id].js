6d54388d3646ec90b969fa4905fff4d552f3d40b
David <ddropping@gmail.com>
2020-10-21 18:21:53 -0700

    Create delete post route

---
+++ pages/api/posts/deletepost/[id].js
@@ -0,0 +1,49 @@
+import connectDb from "../../../../utils/ConnectDb";
+import Post from "../../../../models/Post";
+import User from "../../../../models/User";
+
+import authenticate from "../../../../middleware/auth";
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case "DELETE":
+      await handleDeleteRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   DELETE api/posts/createpost
+// @desc    delete a post
+// @res
+// @access  Protected
+async function handleDeleteRequest(req, res) {
+  const {
+    query: { id },
+  } = req;
+
+  try {
+    //get post
+    let postData = await Post.findById(id);
+
+    //compare post author id to user id
+    if (postData.user.toString() === req.user.id.toString()) {
+      //delete post from db
+      console.log("User Credentials Passed");
+      await Post.findByIdAndDelete(id);
+      res.status(200).send("Delete Successful");
+    } else {
+      res
+        .status(403)
+        .send("Invalid Request: User does not have delete permissions");
+    }
+  } catch (err) {
+    re.status(500).send("Server Error");
+  }
+}
+
+export default authenticate(handler);
