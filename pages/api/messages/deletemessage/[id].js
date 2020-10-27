c5b89f5c44d18a14222ce1d10ab377e2e61ba96e
David <ddropping@gmail.com>
2020-10-27 15:31:04 -0700

    Create deletemessage route

---
+++ pages/api/messages/deletemessage/[id].js
@@ -0,0 +1,40 @@
+import connectDb from "../../../../utils/ConnectDb";
+import Post from "../../../../models/Post";
+import User from "../../../../models/User";
+
+import authenticate from "../../../../middleware/auth";
+
+connectDb();
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case "PATCH":
+      await handlePatchRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+// @route   PATCH api/messages/deletemessage/[id]
+// @desc    remove message _id from user's messages array
+// @res
+// @access  Protected
+async function handlePatchRequest(req, res) {
+  const {
+    query: { id },
+  } = req;
+
+  try {
+    //update user model
+    let user = await User.findByIdAndUpdate(req.user.id, {
+      $pull: { messages: id },
+    });
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+}
+
+export default authenticate(handler);
