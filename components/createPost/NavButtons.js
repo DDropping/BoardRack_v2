c9a991777d93e463602ed401966618d5e9ccd2e4
David <ddropping@gmail.com>
2020-10-30 14:58:38 -0700

    Fix disable publish when loading location image

--- components/createPost/NavButtons.js
+++ components/createPost/NavButtons.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Router from 'next/router';
-import { useDispatch, useSelector } from 'react-redux';
-import { CANCEL_POST } from '../../actions/types';
-import styled from 'styled-components';
-import { Button, Modal } from 'antd';
+import React from "react";
+import Router from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { CANCEL_POST } from "../../actions/types";
+import styled from "styled-components";
+import { Button, Modal } from "antd";
 import {
   LeftOutlined,
   RightOutlined,
-  ExclamationCircleOutlined
-} from '@ant-design/icons';
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
 
-import { publishPost } from '../../actions/publishPost';
+import { publishPost } from "../../actions/publishPost";
 
 const { confirm } = Modal;
 
@@ -20,23 +20,26 @@ const Container = styled.section`
 
 const NavButtons = ({ step, handleStepChange }) => {
   const dispatch = useDispatch();
-  const location = useSelector(state => state.currentLocation.location);
-  const imgList = useSelector(state => state.imgUpload.imgList);
-  const formData = useSelector(state => state.createPostForm);
-  const isLoading = useSelector(state => state.createPostForm.isLoading);
+  const location = useSelector((state) => state.currentLocation.location);
+  const imgList = useSelector((state) => state.imgUpload.imgList);
+  const formData = useSelector((state) => state.createPostForm);
+  const isLoading = useSelector((state) => state.createPostForm.isLoading);
+  const isMapLoading = useSelector(
+    (state) => state.currentLocation.isMapLoading
+  );
 
   function handleCancelConfirm() {
     confirm({
-      title: 'Cancel Post',
+      title: "Cancel Post",
       icon: <ExclamationCircleOutlined />,
-      content: 'Are you sure you want to cancel this post?',
-      okText: 'Yes',
-      okType: 'danger',
-      cancelText: 'No',
+      content: "Are you sure you want to cancel this post?",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
       onOk() {
         dispatch({ type: CANCEL_POST });
-        Router.push('/');
-      }
+        Router.push("/");
+      },
     });
   }
 
@@ -50,7 +53,7 @@ const NavButtons = ({ step, handleStepChange }) => {
         onClick={handleCancelConfirm}
         type="danger"
         ghost
-        style={{ margin: '0.5rem' }}
+        style={{ margin: "0.5rem" }}
       >
         Cancel
       </Button>
@@ -58,11 +61,11 @@ const NavButtons = ({ step, handleStepChange }) => {
       {step !== 0 && (
         <Button
           onClick={() => handleStepChange(step - 1)}
-          style={{ marginRight: '5px' }}
+          style={{ marginRight: "5px" }}
           type="primary"
           ghost
           disabled={step < 1 ? true : false}
-          style={{ margin: '0.5rem' }}
+          style={{ margin: "0.5rem" }}
         >
           <LeftOutlined />
           Previous
@@ -77,7 +80,7 @@ const NavButtons = ({ step, handleStepChange }) => {
           type="primary"
           ghost
           disabled={step > 1 ? true : false}
-          style={{ margin: '0.5rem' }}
+          style={{ margin: "0.5rem" }}
         >
           Next
           <RightOutlined />
@@ -90,9 +93,13 @@ const NavButtons = ({ step, handleStepChange }) => {
           loading={isLoading}
           type="primary"
           disabled={
-            !formData.title || !formData.price || !location.lat || !location.lng
+            !formData.title ||
+            !formData.price ||
+            !location.lat ||
+            !location.lng ||
+            isMapLoading
           }
-          style={{ margin: '0.5rem' }}
+          style={{ margin: "0.5rem" }}
         >
           Publish
         </Button>
