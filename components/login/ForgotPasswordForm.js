802735ab1fd475ad9ba8d4ab9a5c3fac7407b693
David <ddropping@gmail.com>
2020-11-13 13:30:43 -0800

    Create forgot password form

---
+++ components/login/ForgotPasswordForm.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { Button, Checkbox, Form, Input } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import { LeftOutlined } from "@ant-design/icons";
+
+import { TOGGLE_REGISTER, TOGGLE_LOGIN } from "../../actions/types";
+import baseUrl from "../../utils/baseUrl";
+
+const InputWrapper = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const A = styled.a`
+  color: ${({ theme }) => theme.secondaryBlue};
+`;
+
+const ForgotPasswordForm = ({ setForgotPasswordVisible }) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  //disable login button if fields are empty
+  useEffect(() => {
+    const emailVaidate = /\S+@\S+\.\S+/;
+    if (emailVaidate.test(email)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [email]);
+
+  //handle submit
+  const handleSubmit = () => {
+    console.log("submit");
+  };
+
+  return (
+    <div>
+      <p style={{ marginBottom: "1em", textAlign: "center" }}>
+        Forgot your password? No problem! Enter your email in the input below to
+        send a recovery link to your registered email address.
+      </p>
+      <InputWrapper>
+        <Input
+          placeholder='Email'
+          prefix={<MailOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+          size='large'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+        />
+      </InputWrapper>
+      <Button
+        type='primary'
+        htmlType='submit'
+        loading={loading}
+        size='large'
+        style={{ width: "100%" }}
+        disabled={disabled}
+        onClick={handleSubmit}
+      >
+        Send Recovery Link
+      </Button>
+      <Button
+        type='link'
+        onClick={() => setForgotPasswordVisible(false)}
+        style={{ marginTop: "1em" }}
+      >
+        <LeftOutlined /> Click here to return to Login
+      </Button>
+    </div>
+  );
+};
+
+export default ForgotPasswordForm;
--- components/login/index.js
+++ components/login/index.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { Modal } from 'antd';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { Modal } from "antd";
+import { useSelector, useDispatch } from "react-redux";
 
-import { TOGGLE_LOGIN } from '../../actions/types';
-import LoginForm from './LoginForm';
+import { TOGGLE_LOGIN } from "../../actions/types";
+import LoginForm from "./LoginForm";
+import ForgotPasswordForm from "./ForgotPasswordForm";
 
 const index = () => {
   const dispatch = useDispatch();
-  const isVisible = useSelector(state => state.overlays.isLogin);
+  const isVisible = useSelector((state) => state.overlays.isLogin);
+  const [isForgotPasswordVisible, setForgotPasswordVisible] = useState(false);
+
   return (
     <Modal
-      title="Login"
+      title={isForgotPasswordVisible ? "Forgot Password" : "Login"}
       visible={isVisible}
       onOk={() => dispatch({ type: TOGGLE_LOGIN, payload: false })}
       onCancel={() => dispatch({ type: TOGGLE_LOGIN, payload: false })}
       footer={null}
       zIndex={1100}
     >
-      <LoginForm />
+      {isForgotPasswordVisible ? (
+        <ForgotPasswordForm
+          setForgotPasswordVisible={setForgotPasswordVisible}
+        />
+      ) : (
+        <LoginForm setForgotPasswordVisible={setForgotPasswordVisible} />
+      )}
     </Modal>
   );
 };
