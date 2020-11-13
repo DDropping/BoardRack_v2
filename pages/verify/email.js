1224fe40786a3da2fd876c667d677abfead417ec
David <ddropping@gmail.com>
2020-11-12 16:55:51 -0800

    Create email verification confirmation page

---
+++ pages/verify/email.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import axios from "axios";
+
+import baseUrl from "../../utils/baseUrl";
+import EmailConfirmation from "../../components/confirmations/emailConfirm";
+
+const email = () => {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(true);
+  const [isSuccess, setSuccess] = useState(null);
+
+  useEffect(() => {
+    try {
+      const updateUser = async () => {
+        try {
+          const url = `${baseUrl}/api/verification/authenticate/verifyEmail?token=${router.query.token}`;
+          await axios.patch(url);
+          setSuccess(true);
+        } catch (err) {
+          setSuccess(false);
+        }
+      };
+      setLoading(true);
+      updateUser();
+    } catch (err) {
+      setSuccess(false);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>BoardRack | Home</title>
+      </Head>
+      <EmailConfirmation isLoading={isLoading} isSuccess={isSuccess} />
+    </div>
+  );
+};
+
+export default email;
