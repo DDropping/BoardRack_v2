e6768d64a7ab27bc31c5f5955480114564f0114f
David <ddropping@gmail.com>
2020-11-16 17:28:59 -0800

    Refactor fetch posts to pages/index

--- components/displayPosts/index.js
+++ components/displayPosts/index.js
@@ -1,30 +1,15 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
 import { Container, Li } from "./style";
-import baseUrl from "../../utils/baseUrl";
 import PostCard from "../postCard";
 import PostModal from "../postModal";
 import LoadingScreenCard from "../loadingScreens/postCard";
 
-const index = () => {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
+const index = ({ posts, isLoading }) => {
   let loadingCards = [];
   for (let i = 0; i < 20; ++i) {
     loadingCards.push(<LoadingScreenCard key={i} />);
   }
-
-  useEffect(() => {
-    async function fetchData() {
-      const url = `${baseUrl}/api/posts/postdetails`;
-      const res = await axios.get(url);
-      setPosts(res.data);
-      setLoading(false);
-    }
-    fetchData();
-  }, []);
 
   return (
     <Container>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import Head from "next/head";
 import styled from "styled-components";
+import axios from "axios";
 
+import baseUrl from "../utils/baseUrl";
 import Carousel from "../components/carousel";
 import FiltersBar from "../components/filtersBar";
 import FiltersBox from "../components/filtersBox";
@@ -13,23 +16,63 @@ const FiltersPostsContainer = styled.div`
   }
 `;
 
-const Home = (props) => (
-  <div>
-    <Head>
-      <title>BoardRack | Home</title>
-    </Head>
-    <div style={{ zIndex: 1, position: "relative" }}>
-      <Carousel />
+const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+  const filters = useSelector((state) => state.filters);
+  const { price, boardType, condition } = filters;
+
+  //fetch lists of posts
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      //set headers for request
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      //stringify the form items
+      const filtersData = { price, boardType, condition };
+      const body = JSON.stringify(filtersData);
+
+      //fetch posts
+      const url = `${baseUrl}/api/posts/postdetails`;
+      const res = await axios.post(url, body, config);
+      setPosts(res.data);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  //automatically fetch list of post on render
+  useEffect(() => {
+    fetchPosts();
+  }, [filters]);
+
+  return (
+    <div>
+      <Head>
+        <title>BoardRack | Home</title>
+      </Head>
+      <div style={{ zIndex: 1, position: "relative" }}>
+        <Carousel />
+      </div>
+      <div style={{ zIndex: 2, position: "relative" }}>
+        <FiltersBar />
+        <FiltersPostsContainer>
+          <FiltersBox />
+          <PostList posts={posts} isLoading={isLoading} />
+        </FiltersPostsContainer>
+      </div>
     </div>
-    <div style={{ zIndex: 2, position: "relative" }}>
-      <FiltersBar />
-      <FiltersPostsContainer>
-        <FiltersBox />
-        <PostList />
-      </FiltersPostsContainer>
-    </div>
-  </div>
-);
+  );
+};
 
 // Home.getInitialProps = async function() {
 //   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
