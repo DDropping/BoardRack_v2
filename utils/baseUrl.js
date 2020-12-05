16c61e629e5ae9b8e238dad791732a6a1ee9d6bb
David <ddropping@gmail.com>
2020-12-04 17:25:14 -0800

    Update baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://board-rack-v2-ipwaawakr.vercel.app/"
+    ? "https://board-rack-v2-ipwaawakr.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
