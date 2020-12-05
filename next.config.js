3c4bfc958e290a8c35837fb4787311735809e513
David <ddropping@gmail.com>
2020-12-04 16:03:12 -0800

    Update next config quotes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less');
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
-const envConfig = require("./env.config")
+const withLess = require("@zeit/next-less");
+const lessToJS = require("less-vars-to-js");
+const fs = require("fs");
+const path = require("path");
+const envConfig = require("./env.config");
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './public/antd-custom.less'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, "./public/antd-custom.less"), "utf8")
 );
 
 module.exports = withLess({
   lessLoaderOptions: {
     javascriptEnabled: true,
-    modifyVars: themeVariables // make your antd custom effective
+    modifyVars: themeVariables, // make your antd custom effective
   },
   env: envConfig,
   webpack: (config, { isServer }) => {
@@ -23,20 +23,20 @@ module.exports = withLess({
       config.externals = [
         (context, request, callback) => {
           if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === 'function') {
+          if (typeof origExternals[0] === "function") {
             origExternals[0](context, request, callback);
           } else {
             callback();
           }
         },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
       ];
 
       config.module.rules.unshift({
         test: antStyles,
-        use: 'null-loader'
+        use: "null-loader",
       });
     }
     return config;
-  }
+  },
 });
